21a599704ff5c5ef657216872dd4e7e6262869fc
Chunghee Hwang <hch0821@gmail.com>
2020-05-09 00:47:36 +0900

    state 예제 추가

--- d. props, state, key/README.md
+++ d. props, state, key/README.md
@@ -2,6 +2,7 @@
 
 ## props
 props는 html의 attribute 값을 동적으로 변경할 수 있도록 하는 값이다.
+다시 말해, 상위 컴포넌트에서 하위 컴포넌트로 값을 전달할 수 있다.
 ```javascript
 class App extends Component {
   render() {
@@ -36,3 +37,32 @@ props를 변경하고 Enter를 누르면, 실시간으로 값을 변경할 수�
 <p align="center">
   <img src="./props.png" >
 </p>
+
+<hr />
+
+## state
+props : 컴포넌트를 이용하는 사용자 및 개발자에게 중요한 값들<br>
+state: 외부에서 알아서는 안 되는 컴포넌트 내부에서 사용되는 값들<br>
+철저히 props와 분리되서 사용된다.
+
+```javascript
+class App extends Component {
+  // 생성자: render 함수가 실행되기 전 state를 초기화
+  //state: 내부적으로 사용하는 값.
+  constructor(props){
+    super(props)
+    this.state = {
+      subject:{title: 'WEB', sub: 'World Wide Web!'}
+    }
+  }
+  render() {
+    // 상위 컴포넌트에서 하위 컴포넌트로 값을 줄 때 props를 사용한다.
+    // 컴포넌트에 props를 넘겨줄 때, 하드코딩이 아닌 state값을 던져준다.
+    return (
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.title}/>
+      </div>
+    );
+  };
+}
+```
--- d. props, state, key/src/App.js
+++ d. props, state, key/src/App.js
@@ -1,49 +1,26 @@
 import React, { Component } from 'react';
+import TOC from './components/TOC'
+import Content from './components/Content'
+import Subject from './components/Subject'
 import './App.css';
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
 
 class App extends Component {
+  // 생성자: 초기화 담당
+  constructor(props){
+    super(props)
+
+    //state: 내부적으로 사용하는 값.
+    this.state = {
+      subject:{title: 'WEB', sub: 'World Wide Web!'}
+    }
+  }
   render() {
-    // 내가 만든 Subject 태그를 사용할 수 있다.
-    // template literal 없이 태그를 사용할 수 있다.
+    // 상위 컴포넌트에서 하위 컴포넌트로 값을 줄 때 props를 사용한다.
+    // 컴포넌트에 props를 넘겨줄 때, 하드코딩이 아닌 state값을 던져준다.
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web"/>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}/>
         <Subject title="React" sub="For UI"/>
         <TOC/>
         <Content title="HTML" desc="HTML is HyperText markup Language"/>
